f6bfb1fe2f85750dd0b9119e7ef1b7a0001e932b
Willem Röpke <willemropke@hotmail.com>
2020-11-20 16:51:53 +0100

    WIP: implementing rollout and getting rid of global variables

--- MONFG.py
+++ MONFG.py
@@ -107,128 +107,149 @@ def reset(opt=False, rand_prob=False):
     epsilon = epsilon_start
 
 
-parser = argparse.ArgumentParser()
+def run_experiment(runs, episodes, payoff_matrix, num_agents, num_objectives):
+    start = time.time()
 
-# Optimistic initialization can encourage exploration
-parser.add_argument('-opt_init', dest='opt_init', action='store_true', help="optimistic initialization")
-parser.add_argument('-game', type=str, default='game1', choices=['game1', 'game2', 'game3', 'game4', 'game5'],
-                    help="which MONFG game to play")
-parser.add_argument('-criterion', type=str, default='ESR', choices=['SER', 'ESR'], help="optimization criterion to use")
-parser.add_argument('-rand_prob', dest='rand_prob', action='store_true', help="rand init for optimization prob")
+    for run in range(runs):
+        print("Starting run ", r)
+        reset(opt_init, rand_prob)
+        action_hist = [[] for ag in range(num_agents)]
+        for episode in range(episodes):
+            do_episode(episode)
+            payoffs = []
+            actions = []
+            for r in range(rollout):
+                actions, payoffs = do_episode(episode)
 
-parser.add_argument('-provide_comms', dest='provide_comms', action='store_true', help="Allow communication")
+            payoff_episode_log1.append([e, r, calc_utility(0, payoffs[0])])
+            payoff_episode_log2.append([e, r, calc_utility(1, payoffs[1])])
+            for i in range(num_agents):
+                action_hist[i].append(selected_actions[i])
+            if e >= 0.9 * num_episodes:
+                state_distribution_log[selected_actions[0], selected_actions[1]] += 1
 
-parser.add_argument('-runs', type=int, default=100, help="number of trials")
-parser.add_argument('-episodes', type=int, default=10000, help="number of episodes")
+            payoffs = []
 
-args = parser.parse_args()
+        # transform action history into probabilities
+        for a, el in enumerate(action_hist):
+            for i in range(len(el)):
+                if i + window < len(el):
+                    count = Counter(el[i:i + window])
+                else:
+                    count = Counter(el[-window:])
+                total = sum(count.values())
+                act_probs = [0, 0, 0]
+                for action in range(num_actions):
+                    act_probs[action] = count[action] / total
+                act_hist_log[a].append([i, r, act_probs[0], act_probs[1], act_probs[2]])
 
-# Extracting the arguments
-game = args.game
-criterion = args.criterion
-num_runs = args.runs
-num_episodes = args.episodes
-provide_comms = args.provide_comms
-rand_prob = args.rand_prob
-opt_init = args.opt_init
+    end = time.time()
+    elapsed_mins = (end - start) / 60.0
+    print("Minutes elapsed: " + str(elapsed_mins))
 
-payoff_matrix = get_payoff_matrix(game)
-communicator = -1
-message = 0
-num_objectives = 2
-num_agents = 2
-num_actions = payoff_matrix.shape[0]
-num_states = num_actions ** num_agents  # Number of possible joint action messages
-agents = []
-selected_actions = [-1, -1]
-payoffs = [-1, -1]
-current_states = [0, 0]
-alpha = 0.05
-alpha_start = 0.05
-alpha_decay = 1
-epsilon = 0.1
-epsilon_start = 0.1
-epsilon_decay = 0.999
-gamma = 0.0  # this should always be zero as a MONFG is a stateless one shot decision problem
-payoff_log = []
 
-payoff_episode_log1 = []
-payoff_episode_log2 = []
-state_distribution_log = np.zeros((num_actions, num_actions))
-action_hist = [[], []]
-act_hist_log = [[], []]
-window = 100
-final_policy_log = [[], []]
+def save_data():
+        path_data = f'data/{criterion}/{game}'
 
-path_data = f'data/{criterion}/{game}'
+        if opt_init:
+            path_data += '/opt_init'
+        else:
+            path_data += '/zero_init'
 
-if opt_init:
-    path_data += '/opt_init'
-else:
-    path_data += '/zero_init'
+        if rand_prob:
+            path_data += '/opt_rand'
+        else:
+            path_data += '/opt_eq'
 
-if rand_prob:
-    path_data += '/opt_rand'
-else:
-    path_data += '/opt_eq'
+        print("Creating data path: " + repr(path_data))
+        mkdir_p(path_data)
 
-print("Creating data path: " + repr(path_data))
-mkdir_p(path_data)
+        info = 'NE_'
 
-start = time.time()
-for r in range(num_runs):
-    print("Starting run ", r)
-    reset(opt_init, rand_prob)
+        if provide_comms:
+            info += 'comm'
+        else:
+            info += 'no_comm'
+
+        columns = ['Episode', 'Trial', 'Payoff']
+        df1 = pd.DataFrame(payoff_episode_log1, columns=columns)
+        df2 = pd.DataFrame(payoff_episode_log2, columns=columns)
+
+        df1.to_csv(f'{path_data}/agent1_{info}.csv', index=False)
+        df2.to_csv(f'{path_data}/agent2_{info}.csv', index=False)
+
+        columns = ['Episode', 'Trial', 'Action 1', 'Action 2', 'Action 3']
+        df1 = pd.DataFrame(act_hist_log[0], columns=columns)
+        df2 = pd.DataFrame(act_hist_log[1], columns=columns)
+
+        df1.to_csv(f'{path_data}/agent1_probs_{info}.csv', index=False)
+        df2.to_csv(f'{path_data}/agent2_probs_{info}.csv', index=False)
+
+        state_distribution_log /= num_runs * (0.1 * num_episodes)
+        df = pd.DataFrame(state_distribution_log)
+        df.to_csv(f'{path_data}/states_{info}.csv', index=False, header=None)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-game', type=str, default='game1', choices=['game1', 'game2', 'game3', 'game4', 'game5'],
+                        help="which MONFG game to play")
+    parser.add_argument('-criterion', type=str, default='SER', choices=['SER', 'ESR'],
+                        help="optimization criterion to use")
+    parser.add_argument('-communicate', action='store_true', help="Allow communication")
+
+    parser.add_argument('-runs', type=int, default=100, help="number of trials")
+    parser.add_argument('-episodes', type=int, default=10000, help="number of episodes")
+    parser.add_argument('-rollout', type=int, default=100, help="Rollout period to test the current strategies")
+
+    # Optimistic initialization can encourage exploration.
+    parser.add_argument('-opt_init', action='store_true', help="optimistic initialization")
+    parser.add_argument('-rand_prob', action='store_true', help="rand init for optimization prob")
+
+    args = parser.parse_args()
+
+    # Extracting the arguments.
+    game = args.game
+    criterion = args.criterion
+    communicate = args.communicate
+    runs = args.runs
+    episodes = args.episodes
+    rollout = args.rollout
+    opt_init = args.opt_init
+    rand_prob = args.rand_prob
+
+    # Starting the experiments.
+    payoff_matrix = get_payoff_matrix(game)
+    data = run_experiment(runs, episodes, rollout, payoff_matrix)
+
+    # Writing the data to disk.
+    save_data()
+    communicator = -1
+    message = 0
+    num_objectives = 2
+    num_agents = 2
+    num_actions = payoff_matrix.shape[0]
+    num_states = num_actions ** num_agents  # Number of possible joint action messages
+    agents = []
+    selected_actions = [-1, -1]
+    payoffs = [-1, -1]
+    current_states = [0, 0]
+    alpha = 0.05
+    alpha_start = 0.05
+    alpha_decay = 1
+    epsilon = 0.1
+    epsilon_start = 0.1
+    epsilon_decay = 0.999
+    gamma = 0.0  # this should always be zero as a MONFG is a stateless one shot decision problem
+    payoff_log = []
+
+    payoff_episode_log1 = []
+    payoff_episode_log2 = []
+    state_distribution_log = np.zeros((num_actions, num_actions))
     action_hist = [[], []]
-    for e in range(num_episodes):
-        do_episode(e)
-        payoff_episode_log1.append([e, r, calc_utility(0, payoffs[0])])
-        payoff_episode_log2.append([e, r, calc_utility(1, payoffs[1])])
-        for i in range(num_agents):
-            action_hist[i].append(selected_actions[i])
-        if e >= 0.9 * num_episodes:
-            state_distribution_log[selected_actions[0], selected_actions[1]] += 1
+    act_hist_log = [[], []]
+    window = 100
+    final_policy_log = [[], []]
 
-        payoffs = []
 
-    # transform action history into probabilities
-    for a, el in enumerate(action_hist):
-        for i in range(len(el)):
-            if i + window < len(el):
-                count = Counter(el[i:i + window])
-            else:
-                count = Counter(el[-window:])
-            total = sum(count.values())
-            act_probs = [0, 0, 0]
-            for action in range(num_actions):
-                act_probs[action] = count[action] / total
-            act_hist_log[a].append([i, r, act_probs[0], act_probs[1], act_probs[2]])
-
-end = time.time()
-elapsed_mins = (end - start) / 60.0
-print("Time elapsed: " + str(elapsed_mins))
-
-info = 'NE_'
-
-if provide_comms:
-    info += 'comm'
-else:
-    info += 'no_comm'
-
-columns = ['Episode', 'Trial', 'Payoff']
-df1 = pd.DataFrame(payoff_episode_log1, columns=columns)
-df2 = pd.DataFrame(payoff_episode_log2, columns=columns)
-
-df1.to_csv(f'{path_data}/agent1_{info}.csv', index=False)
-df2.to_csv(f'{path_data}/agent2_{info}.csv', index=False)
-
-columns = ['Episode', 'Trial', 'Action 1', 'Action 2', 'Action 3']
-df1 = pd.DataFrame(act_hist_log[0], columns=columns)
-df2 = pd.DataFrame(act_hist_log[1], columns=columns)
-
-df1.to_csv(f'{path_data}/agent1_probs_{info}.csv', index=False)
-df2.to_csv(f'{path_data}/agent2_probs_{info}.csv', index=False)
-
-state_distribution_log /= num_runs * (0.1 * num_episodes)
-df = pd.DataFrame(state_distribution_log)
-df.to_csv(f'{path_data}/states_{info}.csv', index=False, header=None)
