7a0d06e029bf218d4acb1637e8853f2d2fda65be
Willem Röpke <willem.ropke@vub.be>
2022-01-27 15:56:08 +0100

    Update function calls to new function names

--- tests/test_br.py
+++ tests/test_br.py
@@ -1,6 +1,6 @@
 import numpy as np
 import unittest
-import mo_gt.games.utility_functions as utility_functions
+import mo_gt.games.utility_functions as uf
 import mo_gt.games.games as games
 import mo_gt.best_response.best_response as br
 
@@ -13,7 +13,7 @@ class TestBestResponse(unittest.TestCase):
         random_strat = np.array([0.24, 0.29, 0.47])
 
         expected_returns = np.array([[1, 2], [3, 4], [5, 6]])
-        u = utility_functions.u5
+        u = uf.u5
         self.assertEqual(br.objective(unif_strat, expected_returns, u), -37)
         self.assertEqual(br.objective(pure_strat, expected_returns, u), -37)
         self.assertEqual(br.objective(random_strat, expected_returns, u), -47.294799999999995)
@@ -36,14 +36,14 @@ class TestBestResponse(unittest.TestCase):
     def test_best_response(self):
         player = 0
         payoff_matrix = games.monfg1[0]
-        u = utility_functions.u5
+        u = uf.u5
 
         joint_strategy1 = [np.array([1 / 3, 1 / 3, 1 / 3]), np.array([1 / 3, 1 / 3, 1 / 3])]
-        test1 = np.around(br.best_response(u, player, payoff_matrix, joint_strategy1), decimals=5)
+        test1 = np.around(br.calc_best_response(u, player, payoff_matrix, joint_strategy1), decimals=5)
         correct1 = np.array([0.33333, 0.33333, 0.33333])
 
         joint_strategy2 = [np.array([1 / 3, 1 / 3, 1 / 3]), np.array([0.25, 0.35, 0.4])]
-        test2 = np.around(br.best_response(u, player, payoff_matrix, joint_strategy2), decimals=5)
+        test2 = np.around(br.calc_best_response(u, player, payoff_matrix, joint_strategy2), decimals=5)
         correct2 = np.array([0., 0., 1.])
 
         np.testing.assert_array_equal(test1, correct1)
