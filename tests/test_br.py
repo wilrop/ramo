fefd6b579b4931610977a97851b96fa56f6eee0c
Willem Röpke <willem.ropke@vub.be>
2022-01-27 16:14:10 +0100

    Add a test for the PSNE algorithm and reformat test_br code

--- tests/test_br.py
+++ tests/test_br.py
@@ -1,14 +1,16 @@
-import numpy as np
 import unittest
-import mo_gt.games.utility_functions as uf
-import mo_gt.games.games as games
+
+import numpy as np
+
 import mo_gt.best_response.best_response as br
+import mo_gt.games.games as games
+import mo_gt.games.utility_functions as uf
 
 
 class TestBestResponse(unittest.TestCase):
 
     def test_objective(self):
-        unif_strat = np.array([1/3, 1/3, 1/3])
+        unif_strat = np.array([1 / 3, 1 / 3, 1 / 3])
         pure_strat = np.array([0, 1, 0])
         random_strat = np.array([0.24, 0.29, 0.47])
 
@@ -22,11 +24,11 @@ class TestBestResponse(unittest.TestCase):
         player = 0
         payoff_matrix = games.monfg1[0]
 
-        joint_strategy1 = [np.array([1/3, 1/3, 1/3]), np.array([1/3, 1/3, 1/3])]
+        joint_strategy1 = [np.array([1 / 3, 1 / 3, 1 / 3]), np.array([1 / 3, 1 / 3, 1 / 3])]
         test1 = np.around(br.calc_expected_returns(player, payoff_matrix, joint_strategy1), decimals=0)
         correct1 = np.array([[3., 1.], [2., 2.], [1., 3.]])
 
-        joint_strategy2 = [np.array([1/3, 1/3, 1/3]), np.array([0.25, 0.35, 0.4])]
+        joint_strategy2 = [np.array([1 / 3, 1 / 3, 1 / 3]), np.array([0.25, 0.35, 0.4])]
         test2 = np.around(br.calc_expected_returns(player, payoff_matrix, joint_strategy2), decimals=2)
         correct2 = np.array([[2.85, 1.15], [1.85, 2.15], [0.85, 3.15]])
 
---
+++ tests/test_psne.py
@@ -0,0 +1,56 @@
+import unittest
+
+import numpy as np
+
+import mo_gt.best_response.PSNE as psne
+import mo_gt.games.games as games
+import mo_gt.games.utility_functions as uf
+
+
+class TestPSNE(unittest.TestCase):
+
+    def test_small_game(self):
+        game_str = 'game5'
+        u_str = ('u6', 'u7')
+
+        game = games.get_monfg(game_str)
+        u_tpl = []
+        for i in range(len(u_str)):
+            u_tpl.append(uf.get_u(u_str[i]))
+        u_tpl = tuple(u_tpl)
+
+        test = psne.calc_all_psne(game, u_tpl)
+        correct = np.array([[0, 0], [1, 1]])
+        np.testing.assert_array_equal(test, correct)
+
+    def test_medium_game(self):
+        game_str = 'game14'
+        u_str = ('u1', 'u5', 'u6')
+
+        game = games.get_monfg(game_str)
+        u_tpl = []
+        for i in range(len(u_str)):
+            u_tpl.append(uf.get_u(u_str[i]))
+        u_tpl = tuple(u_tpl)
+
+        test = psne.calc_all_psne(game, u_tpl)
+        correct = np.array([[0, 1, 1], [1, 0, 1]])
+        np.testing.assert_array_equal(test, correct)
+
+    def test_large_game(self):
+        game_str = 'game15'
+        u_str = ('u1', 'u5', 'u6')
+
+        game = games.get_monfg(game_str)
+        u_tpl = []
+        for i in range(len(u_str)):
+            u_tpl.append(uf.get_u(u_str[i]))
+        u_tpl = tuple(u_tpl)
+
+        test = psne.calc_all_psne(game, u_tpl)
+        correct = np.array([[0, 1, 1], [1, 0, 2], [1, 1, 0]])
+        np.testing.assert_array_equal(test, correct)
+
+
+if __name__ == '__main__':
+    unittest.main()
