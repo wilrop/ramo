801c03622377fa77eb61ff8f3adb0e6f4edc7cbd
Willem Röpke <willem.ropke@vub.be>
2022-01-27 15:26:10 +0100

    Update documentation and minor code fixes

--- mo_gt/best_response/Player.py
+++ mo_gt/best_response/Player.py
@@ -1,9 +1,10 @@
 import numpy as np
-from mo_gt.best_response.best_response import calculate_best_response
+
+from mo_gt.best_response.best_response import calc_best_response
 
 
 class IBRPlayer:
-    """A class implementing a player that learns using the iterated best response algorithm."""
+    """A player that learns using the iterated best response algorithm."""
 
     def __init__(self, id, u, num_actions, payoff_matrix, init_strategy=None):
         self.id = id
@@ -16,17 +17,16 @@ class IBRPlayer:
             self.strategy = init_strategy
 
     def update_strategy(self, joint_strategy):
-        """This function updates the current strategy of the player by calculating a best response to the other players'
-        strategies.
+        """Update the strategy by calculating a best response to the other players' strategies.
 
         Args:
-          joint_strategy: The current joint strategy of all players.
+          joint_strategy (List[ndarray]): A list of each player's individual strategy.
 
         Returns:
-          Whether the strategy has converged and the best response strategy.
+          Tuple(bool, ndarray): Whether the strategy has converged and the best response strategy.
 
         """
-        br = best_response(self.u, self.id, self.payoff_matrix, joint_strategy)
+        br = calc_best_response(self.u, self.id, self.payoff_matrix, joint_strategy)
         if (br == self.strategy).all():
             done = True
         else:
@@ -36,7 +36,7 @@ class IBRPlayer:
 
 
 class FPPlayer:
-    """A class implementing a player that learns using fictitious play."""
+    """A player that learns using the fictitious play algorithm."""
 
     def __init__(self, id, u, player_actions, payoff_matrix, init_strategy=None):
         self.id = id
@@ -51,23 +51,19 @@ class FPPlayer:
         self.empirical_strategies = [np.zeros(num_actions) for num_actions in player_actions]
 
     def select_action(self):
-        """Select a random action using the current strategy.
-        :return: An action.
-
-        Args:
+        """Select an action using the current strategy.
 
         Returns:
+            int: The selected action.
 
         """
         return np.random.choice(range(self.num_actions), p=self.strategy)
 
     def calc_joint_strategy(self):
-        """This function calculates the empirical joint strategy.
-        :return: The joint strategy.
-
-        Args:
+        """Calculates the empirical joint strategy.
 
         Returns:
+            List[ndarray]: The joint strategy.
 
         """
         joint_strategy = []
@@ -77,16 +73,14 @@ class FPPlayer:
         return joint_strategy
 
     def update_strategy(self):
-        """This function updates the strategy of the player by calculating a best response to the empirical joint strategy.
-        :return: Whether the strategy has converged and the best response strategy.
-
-        Args:
+        """Updates the strategy of the player by calculating a best response to the empirical joint strategy.
 
         Returns:
+            Tuple(bool, ndarray): Whether the strategy has converged and the best response strategy.
 
         """
         joint_strategy = self.calc_joint_strategy()
-        br = best_response(self.u, self.id, self.payoff_matrix, joint_strategy)
+        br = calc_best_response(self.u, self.id, self.payoff_matrix, joint_strategy)
         if (br == self.strategy).all():
             done = True
         else:
@@ -94,13 +88,11 @@ class FPPlayer:
         self.strategy = br
         return done, br
 
-    def update_joint_states(self, actions):
+    def update_empirical_strategies(self, actions):
         """Update the empirical strategy of all players.
 
         Args:
-          actions: The actions that are taken by the players.
-
-        Returns:
+          actions (List[int]): The actions that were taken by the players.
 
         """
         for player, action in enumerate(actions):
