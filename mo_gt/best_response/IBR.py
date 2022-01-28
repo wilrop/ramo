e29a1301fb90b9cd05f9b92d80b4900502bd5229
Willem Röpke <willem.ropke@vub.be>
2022-01-28 12:03:16 +0100

    Minor code updates and documentation improvements

--- mo_gt/best_response/IBR.py
+++ mo_gt/best_response/IBR.py
@@ -1,33 +1,40 @@
 import argparse
-import time
 import copy
-import games
-import util
+import time
 
-from Player import IBRPlayer
+import mo_gt.games.games as games
+import mo_gt.games.utility_functions as uf
+import mo_gt.utils.printing as pt
+from mo_gt.best_response.Player import IBRPlayer
 
 
-def iterated_best_response(u_tpl, player_actions, monfg, max_iter=1000, init_joint_strategy=None,
-                           variant='simultaneous'):
-    """This function executes the iterated best response algorithm on a given MONFG with accompanied utility functions.
-    Note that the simultaneous and alternating variants are not equivalent in general. In the simultaneous variant, all
-    players calculate their best-response strategy simultaneously. The alternating variant does it by alternating.
-    Note that at this point in time, this algorithm does not find cycles in the IBR dynamic.
+def iterated_best_response(u_tpl, monfg, max_iter=1000, init_joint_strategy=None, variant='simultaneous'):
+    """Execute the iterated best response algorithm on a given MONFG and utility functions.
+
+    There are two variants of the iterated best response algorithm implemented, a simultaneous and alternating variant.
+    These are not equivalent in general. In the simultaneous variant, all players calculate their best-response
+    strategy simultaneously. The alternating variant does it by alternating.
+
+    Notes
+    -----
+    At this point in time, the algorithm does not find cycles and will continue to execute until the maximum number of
+    iterations is reached.
 
     Args:
-      u_tpl: A tuple of utility functions.
-      player_actions: A tuple of actions per player.
-      monfg: A list of payoff matrices representing the MONFG.
-      max_iter: The maximum amount of iterations to run IBR for. (Default value = 1000)
-      init_joint_strategy: Initial guess for the joint strategy. (Default value = None)
-      variant: The variant to use. This is either simultaneous or alternating. (Default value = 'simultaneous')
+      u_tpl (Tuple[callable]): A tuple of utility functions.
+      monfg (List[ndarray]): A list of payoff matrices representing the MONFG.
+      max_iter (int, optional): The maximum amount of iterations to run IBR for. (Default value = 1000)
+      init_joint_strategy (List[ndarray], optional): Initial guess for the joint strategy. (Default value = None)
+      variant (str, optional): The variant to use, which is either simultaneous or alternating.
+        (Default value = 'simultaneous')
 
     Returns:
-      Whether or not we reached a Nash equilibrium and the final joint strategy.
+      Tuple[bool, List[ndarray]]: Whether or not we reached a Nash equilibrium and the final joint strategy.
 
     """
-    util.print_start('Iterated Best Response')
+    pt.print_start('Iterated Best Response')
 
+    player_actions = monfg[0].shape[:-1]  # Get the number of actions available to each player.
     players = []  # A list to hold all the players.
     joint_strategy = []  # A list to hold the current joint strategy.
 
@@ -44,14 +51,12 @@ def iterated_best_response(u_tpl, player_actions, monfg, max_iter=1000, init_joi
     nash_equilibrium = False  # The current joint strategy is not known to be a Nash equilibrium at this point.
     new_joint_strategy = copy.deepcopy(joint_strategy)
     if variant == 'simultaneous':
-        # We hide the strategy updates of other players until everyone is finished. This makes it a simultaneous update.
         def update_strategy():
-            """ """
+            """Hide the strategy updates of other players until everyone is finished for the simultaneous update."""
             return joint_strategy
     else:
-        # We show the strategy updates of other players. This makes it an alternating update.
         def update_strategy():
-            """ """
+            """Show the strategy updates of other players for the alternating update."""
             return new_joint_strategy
 
     for i in range(max_iter):
@@ -76,14 +81,10 @@ def iterated_best_response(u_tpl, player_actions, monfg, max_iter=1000, init_joi
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--game', type=str, default='game1',
-                        choices=['game1', 'game2', 'game3', 'game4', 'game5', 'game6', 'game7', 'game8', 'game9',
-                                 'random'],
-                        help="which MONFG to play")
+    parser.add_argument('--game', type=str, default='game1', help="which MONFG to play")
     parser.add_argument('--variant', type=str, default='alternating', choices=['simultaneous', 'alternating'])
     parser.add_argument('--iterations', type=int, default=1000, help="The maximum number of iterations.")
-    parser.add_argument('-u', type=str, default=['u1', 'u2'], choices=['u1', 'u2', 'u3', 'u4'], nargs='+',
-                        help="Which utility functions to use per player.")
+    parser.add_argument('-u', type=str, default=['u1', 'u2'], nargs='+', help="The utility functions to use.")
     parser.add_argument('--player_actions', type=int, nargs='+', default=[5, 5],
                         help='The number of actions per player')
     parser.add_argument('--num_objectives', type=int, default=2, help="The number of objectives for the random MONFG.")
@@ -100,14 +101,12 @@ if __name__ == '__main__':
     else:
         monfg = games.get_monfg(args.game)
 
-    player_actions = monfg[0].shape[:-1]  # Get the number of actions available to each player.
-    u_tpl = tuple([games.get_u(u_str) for u_str in args.u])
+    u_tpl = tuple([uf.get_u(u_str) for u_str in args.u])
     variant = args.variant
     iterations = args.iterations
 
-    # guess = [np.array([0, 1]), np.array([0, 1])]
-    ne, final_strategy = iterated_best_response(u_tpl, player_actions, monfg, max_iter=iterations, variant=variant)
-    util.print_ne(ne, final_strategy)
+    ne, final_strategy = iterated_best_response(u_tpl, monfg, max_iter=iterations, variant=variant)
+    pt.print_ne(ne, final_strategy)
 
     end = time.time()
     elapsed_secs = (end - start)
