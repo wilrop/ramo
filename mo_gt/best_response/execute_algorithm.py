a3a5baee45d1c32d81d992d4c00ce8b24c308542
Willem Röpke <willem.ropke@vub.be>
2022-01-28 14:26:36 +0100

    Rename execute algorithm file, code cleanup and improve documentation

--- mo_gt/best_response/execute_algorithm.py
+++ mo_gt/best_response/execute_algorithm.py
@@ -1,60 +1,52 @@
 import time
 
 import numpy as np
+import mo_gt.utils.printing as pt
 
-import mo_gt.games.games as games
+from mo_gt.best_response.PSNE import calc_all_psne
 from mo_gt.best_response.IBR import iterated_best_response
 from mo_gt.best_response.fictitious_play import fictitious_play
 
 
-def execute_algorithm(algorithm='PSNE', game='game1', u=('u1', 'u1'), player_actions=(5, 5), variant='alternating',
-                      iterations=1000, num_objectives=2, lower_bound=0, upper_bound=5, seed=None):
+def execute_algorithm(monfg, u_tpl, algorithm='PSNE', variant='alternating', iterations=1000, seed=None):
     """Execute the requested algorithm with the arguments provided by the user.
 
     Args:
-        algorithm:
-        game:
-        u:
-        player_actions:
-        variant:
-        iterations:
-        num_objectives:
-        lower_bound:
-        upper_bound:
-        seed:
+        monfg (List[ndarray]): A list of payoff matrices representing the MONFG.
+        u_tpl (Tuple[callable]): A tuple of utility functions.
+        algorithm (str, optional): The requested algorithm. (Default value = 'PSNE')
+        variant (str, optional): The variant to use when executing fictitious play or iterated best response.
+            (Default value = 'alternating')
+        iterations (int, optional): The number of iterations to execute. Only used when executing fictitious play or
+            iterated best response. (Default value = 1000)
+        seed (int, optional): Seed the NumPy generator. If set to None, the system seed is used. (Default = None)
 
     Returns:
+        ndarray | Tuple[bool, List[ndarray]]: The return from the requested algorithm.
+
+    Raises:
+        Exception: When the requested algorithm is unknown.
 
     """
     start = time.time()  # Start measuring the time.
 
-    if args.seed is not None:
-        np.random.seed(args.seed)  # Set the numpy seed.
-
-    if args.game == 'random':  # We get the game
-        player_actions = tuple(args.player_actions)
-        monfg = games.generate_random_monfg(player_actions, args.num_objectives, args.lower_bound, args.upper_bound)
-    else:
-        monfg = games.get_monfg(args.game)
-
-    player_actions = monfg[0].shape[:-1]  # Get the number of actions available to each player.
-    u_tpl = tuple([games.get_u(u_str) for u_str in args.u])
-
-    algorithm = args.algorithm
-    variant = args.variant
-    iterations = args.iterations
+    if seed is not None:
+        np.random.seed(seed)  # Set the numpy seed.
 
     if algorithm == 'PSNE':
-        psne_lst = find_all_psne(monfg, player_actions, u_tpl)
-        util.print_psne(psne_lst)
+        psne_lst = calc_all_psne(monfg, u_tpl)
+        pt.print_psne(monfg[0], psne_lst)
+        return psne_lst
     elif algorithm == 'IBR':
-        ne, final_strategy = iterated_best_response(u_tpl, player_actions, monfg, max_iter=iterations, variant=variant)
-        util.print_ne(ne, final_strategy)
+        ne, final_strategy = iterated_best_response(monfg, u_tpl, max_iter=iterations, variant=variant)
+        pt.print_ne(ne, final_strategy)
+        return ne, final_strategy
     elif algorithm == 'FP':
-        ne, final_strategy = fictitious_play(u_tpl, player_actions, monfg, max_iter=iterations, variant=variant)
-        util.print_ne(ne, final_strategy)
+        ne, final_strategy = fictitious_play(monfg, u_tpl, max_iter=iterations, variant=variant)
+        pt.print_ne(ne, final_strategy)
+        return ne, final_strategy
     else:
-        raise Exception('The requested algorithm does not exist')
+        raise Exception(f'The requested algorithm "{algorithm}" does not exist')
 
     end = time.time()
     elapsed_secs = (end - start)
