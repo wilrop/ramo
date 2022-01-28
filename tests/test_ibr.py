dce720ed8150b69ef2e7e55bdd849fcdebc66d32
Willem Röpke <willem.ropke@vub.be>
2022-01-28 14:25:51 +0100

    Update iterated best response tests

--- tests/test_ibr.py
+++ tests/test_ibr.py
@@ -7,10 +7,11 @@ import mo_gt.games.games as games
 import mo_gt.games.utility_functions as uf
 
 
-class TestPSNE(unittest.TestCase):
-    np.random.seed(42)
+class TestIteratedBestResponse(unittest.TestCase):
 
     def test_alternating(self):
+        np.random.seed(42)
+
         game_str = 'game14'
         u_str = ('u1', 'u2', 'u3')
 
@@ -20,13 +21,15 @@ class TestPSNE(unittest.TestCase):
             u_tpl.append(uf.get_u(u_str[i]))
         u_tpl = tuple(u_tpl)
 
-        test = ibr.iterated_best_response(u_tpl, game, variant='alternating')
+        test = ibr.iterated_best_response(game, u_tpl, variant='alternating')
         self.assertTrue(test[0])
         correct = [np.array([0., 1.]), np.array([0., 1.]), np.array([1., 0.])]
         for idx, strategy in enumerate(test[1]):
             np.testing.assert_array_equal(np.round(strategy, decimals=0), correct[idx])
 
     def test_simultaneous(self):
+        np.random.seed(42)
+
         game_str = 'game14'
         u_str = ('u1', 'u2', 'u3')
 
@@ -36,7 +39,7 @@ class TestPSNE(unittest.TestCase):
             u_tpl.append(uf.get_u(u_str[i]))
         u_tpl = tuple(u_tpl)
 
-        test = ibr.iterated_best_response(u_tpl, game, variant='simultaneous')
+        test = ibr.iterated_best_response(game, u_tpl, variant='simultaneous')
         self.assertTrue(test[0])
         correct = [np.array([0., 1.]), np.array([0., 1.]), np.array([1., 0.])]
         for idx, strategy in enumerate(test[1]):
