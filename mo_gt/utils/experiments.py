63deec514f43774b2846bbc2fa4061f819070f27
Willem Röpke <willem.ropke@vub.be>
2022-01-31 17:23:42 +0100

    Implement helper function to slice an array dynamically through an axis

--- mo_gt/utils/experiments.py
+++ mo_gt/utils/experiments.py
@@ -26,3 +26,23 @@ def create_game_path(content, experiment, game, opt_init, mkdir=False):
         os.makedirs(path, exist_ok=True)
 
     return path
+
+
+def array_slice(array, axis, start, end, step=1):
+    """Slice an array across a desired index.
+
+    Notes
+    ------
+    See: https://stackoverflow.com/questions/24398708/slicing-a-numpy-array-along-a-dynamically-specified-axis
+
+    Args:
+        array (ndarray): An input array.
+        axis (int): The axis to slice through.
+        start (int): The start index of that axis.
+        end (int): The end index of that axis.
+        step (int): The step size of the slice.
+
+    Returns:
+
+    """
+    return array[(slice(None),) * (axis % array.ndim) + (slice(start, end, step),)]
