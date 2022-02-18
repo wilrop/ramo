20c8387336887a8f5978bc9bb7432676bbf112df
Willem Röpke <willem.ropke@vub.be>
2022-02-18 15:27:05 +0100

    Create helper functions

---
+++ mo_gt/utils/helpers.py
@@ -0,0 +1,18 @@
+def array_slice(array, axis, start, end, step=1):
+    """Slice an array across a desired index.
+
+    Notes
+    ------
+    See: https://stackoverflow.com/questions/24398708/slicing-a-numpy-array-along-a-dynamically-specified-axis
+
+    Args:
+        array (ndarray): An input array.
+        axis (int): The axis to slice through.
+        start (int): The start index of that axis.
+        end (int): The end index of that axis.
+        step (int): The step size of the slice.
+
+    Returns:
+
+    """
+    return array[(slice(None),) * (axis % array.ndim) + (slice(start, end, step),)]
