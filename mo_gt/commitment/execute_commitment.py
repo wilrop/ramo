af5f6c382cd2218245953065685a02ae254a11d1
Willem Röpke <willem.ropke@vub.be>
2022-03-08 09:43:55 +0100

    Separate agent loader to avoid circular imports

--- mo_gt/commitment/execute_commitment.py
+++ mo_gt/commitment/execute_commitment.py
@@ -6,8 +6,8 @@ import numpy as np
 
 import mo_gt.games.games as games
 from mo_gt.utils.data import save_metadata, save_data
-from mo_gt.utils.experiments import create_game_path, calc_com_probs, calc_returns, calc_action_probs, get_payoffs, \
-    create_agents
+from mo_gt.utils.agent_loader import create_agents
+from mo_gt.utils.experiments import create_game_path, calc_com_probs, calc_returns, calc_action_probs, get_payoffs
 
 
 def get_leader(agents, episode, alternate=False):
