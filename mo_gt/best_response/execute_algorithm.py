d1a142a2ef99ad2e888e79f691aaaa9ea2f98055
Willem Röpke <willem.ropke@vub.be>
2022-03-11 11:49:37 +0100

    Update algorithm executor

--- mo_gt/best_response/execute_algorithm.py
+++ mo_gt/best_response/execute_algorithm.py
@@ -5,18 +5,16 @@ from mo_gt.best_response.PSNE import calc_all_psne
 from mo_gt.best_response.fictitious_play import fictitious_play
 
 
-def execute_algorithm(monfg, u_tpl, algorithm='PSNE', variant='alternating', iterations=1000, seed=None):
+def execute_algorithm(monfg, u_tpl, algorithm='PSNE', seed=None, options=None):
     """Execute the requested algorithm with the arguments provided by the user.
 
     Args:
         monfg (List[ndarray]): A list of payoff matrices representing the MONFG.
         u_tpl (Tuple[callable]): A tuple of utility functions.
         algorithm (str, optional): The requested algorithm. (Default value = 'PSNE')
-        variant (str, optional): The variant to use when executing fictitious play or best-response iteration.
-            (Default value = 'alternating')
-        iterations (int, optional): The number of iterations to execute. Only used when executing fictitious play or
-            iterated best response. (Default value = 1000)
         seed (int, optional): Seed the NumPy generator. If set to None, the system seed is used. (Default = None)
+        options (Dict, optional): A dictionary of options for the selected algorithm. For a complete overview of the
+            arguments per algorithm, see the algorithm documentation. (Default value = None)
 
     Returns:
         ndarray | Tuple[bool, List[ndarray]]: The return from the requested algorithm.
@@ -31,10 +29,10 @@ def execute_algorithm(monfg, u_tpl, algorithm='PSNE', variant='alternating', ite
         psne_lst = calc_all_psne(monfg, u_tpl)
         results = psne_lst
     elif algorithm == 'IBR':
-        ne, final_strategy = iterated_best_response(monfg, u_tpl, max_iter=iterations, variant=variant, seed=seed)
+        ne, final_strategy = iterated_best_response(monfg, u_tpl, seed=seed, **options)
         results = ne, final_strategy
     elif algorithm == 'FP':
-        ne, final_strategy = fictitious_play(monfg, u_tpl, max_iter=iterations, variant=variant, seed=seed)
+        ne, final_strategy = fictitious_play(monfg, u_tpl, seed=seed, **options)
         results = ne, final_strategy
     else:
         raise Exception(f'The requested algorithm "{algorithm}" does not exist')
