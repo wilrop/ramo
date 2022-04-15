81bbd2e190d8b294b941f59e8834efd9fef2ac55
Willem Röpke <willem.ropke@vub.be>
2022-04-15 13:04:59 +0200

    Implement strict verification that returns false when the global optimiser was unsuccessful

--- mo_gt/best_response/best_response.py
+++ mo_gt/best_response/best_response.py
@@ -173,7 +173,7 @@ def calc_best_response(u, player, payoff_matrix, joint_strategy, epsilon=0, glob
     return br_strategy
 
 
-def verify_nash(monfg, u_tpl, joint_strat, epsilon=0, tol=1e-12):
+def verify_nash(monfg, u_tpl, joint_strat, epsilon=0, tol=1e-12, strict=False):
     """Verify whether the joint strategy is a Nash equilibrium
 
     Args:
@@ -183,12 +183,14 @@ def verify_nash(monfg, u_tpl, joint_strat, epsilon=0, tol=1e-12):
         epsilon (float, optional): An optional parameter to allow for approximate Nash equilibria. (Default value = 0)
         tol (float, optional): The tolerance in the utility calculation. The default is set to the shgo default from
         SciPy. (Default value = 1e-12)
+        strict (bool, optional): Whether to count unsuccessful optimisations as unverified and thus returning False.
+        (Default value = False)
 
-    Notes:
+    Note:
         A Nash equilibrium occurs whenever all strategies are best-responses to each other. We specifically use a global
-        optimiser in this function to ensure all strategies are really best-responses and not local optima. Note that
-        finding a global optimum for a function is computationally expensive, so this function might take longer than
-        expected.
+        optimiser in this function to ensure all strategies are really best-responses and not local optima. Be aware
+        that finding a global optimum for a function is computationally expensive, so this function might take longer
+        than expected.
 
     Returns:
         bool: Whether the given joint strategy is a Nash equilibrium.
@@ -197,7 +199,7 @@ def verify_nash(monfg, u_tpl, joint_strat, epsilon=0, tol=1e-12):
         expected_returns = calc_expected_returns(player, payoffs, joint_strat)
         utility_from_strat = objective(strat, expected_returns, u)
         success, br_strat, br_utility = optimise_policy(expected_returns, u, global_opt=True)
-        if not (success and (utility_from_strat + epsilon + tol >= br_utility)):
+        if (not strict or success) and utility_from_strat + epsilon + tol < br_utility:
             return False
     return True
 
