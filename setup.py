370228627b55737d4f856124bc840d191fc9c65b
Willem Röpke <willem.ropke@vub.be>
2022-06-24 16:52:11 +0200

    Make setup file to publish ramo as a pip package

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Ramo",  # The name of the package.
+    version="0.0.1",  # The current release version.
+    author="Willem Röpke",  # The full name of the author.
+    author_email="willem.ropke@vub.be",  # Email address of the author.
+    description="Algorithms for computing or learning equilibria multi-objective games",  # Short tagline.
+    long_description=long_description,  # Long description read from the readme file.
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),  # List of all python modules to be installed.
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],  # Information to filter the project on PyPi website.
+    python_requires='>=3.6',  # Minimum version requirement of the package.
+    py_modules=["ramo"],  # Name of the python package.
+    install_requires=['scipy']  # Install other dependencies if any.
+)
