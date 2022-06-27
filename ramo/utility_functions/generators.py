e67feb49e11497b8ac2c145a813890c3229c7fb0
Willem Röpke <willem.ropke@vub.be>
2022-06-27 15:29:23 +0200

    Comment constant utility function generator

--- ramo/utility_functions/generators.py
+++ ramo/utility_functions/generators.py
@@ -1,4 +1,12 @@
 def constant_u(k):
+    """Create a utility function that always outputs a constant value k.
+
+    Args:
+        k (float): The constant output of the utility function.
+
+    Returns:
+        callable: The generated utility function.
+    """
     def u(x):
         return k
     return u
