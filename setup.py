d9e321e17e0bbf59a24cdab4ab4ee7a63e519432
Willem Röpke <willem.ropke@vub.be>
2022-10-31 16:54:21 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version="0.0.1",  # The current release version.
     author="Willem Röpke",  # The full name of the author.
     author_email="willem.ropke@vub.be",  # Email address of the author.
-    description="Algorithms for computing or learning equilibria multi-objective games",  # Short tagline.
+    description="Algorithms for computing or learning equilibria in multi-objective games",  # Short tagline.
     long_description=long_description,  # Long description read from the readme file.
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),  # List of all python modules to be installed.
@@ -19,5 +19,4 @@ setuptools.setup(
     ],  # Information to filter the project on PyPi website.
     python_requires='>=3.6',  # Minimum version requirement of the package.
     py_modules=["ramo"],  # Name of the python package.
-    install_requires=['scipy']  # Install other dependencies if any.
 )
