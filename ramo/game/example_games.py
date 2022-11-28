4b03a5900c0c6859f56b90bc1e15dfba8ae36b93
Willem Röpke <willem.ropke@vub.be>
2022-11-28 17:49:54 +0000

    Update the files to acount for new MONFG object

--- ramo/game/example_games.py
+++ ramo/game/example_games.py
@@ -1,41 +1,43 @@
 import numpy as np
 
-monfg1 = [np.array([[(4, 0), (3, 1), (2, 2)],
-                    [(3, 1), (2, 2), (1, 3)],
-                    [(2, 2), (1, 3), (0, 4)]], dtype=float),
-          np.array([[(4, 0), (3, 1), (2, 2)],
-                    [(3, 1), (2, 2), (1, 3)],
-                    [(2, 2), (1, 3), (0, 4)]], dtype=float)]
+from ramo.game.monfg import Monfg
+
+payoffs1 = [np.array([[(4, 0), (3, 1), (2, 2)],
+                      [(3, 1), (2, 2), (1, 3)],
+                      [(2, 2), (1, 3), (0, 4)]], dtype=float),
+            np.array([[(4, 0), (3, 1), (2, 2)],
+                      [(3, 1), (2, 2), (1, 3)],
+                      [(2, 2), (1, 3), (0, 4)]], dtype=float)]
 """List[ndarray]: Game 1 - The (im)balancing act game. A 3-action 2-player game with team rewards.
 
 There are no NE under SER using u1 and u2.
 There are two PSNE using u1 and u5: [0, 0], [2, 2]. Checked for correctness using Gambit.
 """
 
-monfg2 = [np.array([[(4, 0), (2, 2)],
-                    [(2, 2), (0, 4)]], dtype=float),
-          np.array([[(4, 0), (2, 2)],
-                    [(2, 2), (0, 4)]], dtype=float)]
+payoffs2 = [np.array([[(4, 0), (2, 2)],
+                      [(2, 2), (0, 4)]], dtype=float),
+            np.array([[(4, 0), (2, 2)],
+                      [(2, 2), (0, 4)]], dtype=float)]
 """List[ndarray]: Game 2 - The (im)balancing act game without M. A 2-action 2-player game with team rewards.
 
 There are no NE under SER using u1 and u2.
 There are two PSNE using u1 and u5: [0, 0], [1, 1]. Checked for correctness using Gambit.
 """
 
-monfg3 = [np.array([[(4, 0), (3, 1)],
-                    [(3, 1), (2, 2)]], dtype=float),
-          np.array([[(4, 0), (3, 1)],
-                    [(3, 1), (2, 2)]], dtype=float)]
+payoffs3 = [np.array([[(4, 0), (3, 1)],
+                      [(3, 1), (2, 2)]], dtype=float),
+            np.array([[(4, 0), (3, 1)],
+                      [(3, 1), (2, 2)]], dtype=float)]
 """List[ndarray]: Game 3 - The (im)balancing act game without R. A 2-action 2-player game with team rewards.
 
 There is one NE under SER using u1 and u2: (L, M).
 There is one PSNE using u1 and u5: [0, 0]. Checked for correctness using Gambit.
 """
 
-monfg4 = [np.array([[(4, 1), (1, 2)],
-                    [(3, 1), (3, 2)]], dtype=float),
-          np.array([[(4, 1), (1, 2)],
-                    [(3, 1), (3, 2)]], dtype=float)]
+payoffs4 = [np.array([[(4, 1), (1, 2)],
+                      [(3, 1), (3, 2)]], dtype=float),
+            np.array([[(4, 1), (1, 2)],
+                      [(3, 1), (3, 2)]], dtype=float)]
 """List[ndarray]: Game 4 - A 2-action 2-player game with team rewards.
 
 There is one NE under SER using u1 and u2: (M, M).
@@ -45,12 +47,12 @@ This game shows cyclic behaviour under IBR with simultaneous updates but not wit
 This game shows no cyclic behaviour with fictitious play.
 """
 
-monfg5 = [np.array([[(4, 1), (1, 2), (2, 1)],
-                    [(3, 1), (3, 2), (1, 2)],
-                    [(1, 2), (2, 1), (1, 3)]], dtype=float),
-          np.array([[(4, 1), (1, 2), (2, 1)],
-                    [(3, 1), (3, 2), (1, 2)],
-                    [(1, 2), (2, 1), (1, 3)]], dtype=float)]
+payoffs5 = [np.array([[(4, 1), (1, 2), (2, 1)],
+                      [(3, 1), (3, 2), (1, 2)],
+                      [(1, 2), (2, 1), (1, 3)]], dtype=float),
+            np.array([[(4, 1), (1, 2), (2, 1)],
+                      [(3, 1), (3, 2), (1, 2)],
+                      [(1, 2), (2, 1), (1, 3)]], dtype=float)]
 """List[ndarray]: Game 5 - A 3-action 2-player game with team rewards.
 
 There is one NE under SER using u1 and u2: (M, M).
@@ -61,63 +63,63 @@ There are three PSNE using u1 and u5: [0, 0], [1, 1], [2, 2]. Checked for correc
 This game shows cyclic behaviour under IBR with simultaneous updates but not with alternating updates.
 """
 
-monfg6 = [np.array([[(0, 0), (7, 2)],
-                    [(2, 7), (6, 2.32502)]], dtype=float),
-          np.array([[(0, 0), (2, 7)],
-                    [(7, 2), (6, 2.32502)]], dtype=float)]
+payoffs6 = [np.array([[(0, 0), (7, 2)],
+                      [(2, 7), (6, 2.32502)]], dtype=float),
+            np.array([[(0, 0), (2, 7)],
+                      [(7, 2), (6, 2.32502)]], dtype=float)]
 """List[ndarray]: Game 6 - A multi-objectivised version of the game of chicken. Both players use the utility function u2.
 
 The cyclic equilibrium is to go 2/3 your own, 1/3 other action uniformly over these.
 """
 
-monfg7 = [np.array([[(-1, -1), (-1, 1)],
-                    [(1, -1), (1, 1)]], dtype=float),
-          np.array([[(-1, -1), (-1, 1)],
-                    [(1, -1), (1, 1)]], dtype=float)]
+payoffs7 = [np.array([[(-1, -1), (-1, 1)],
+                      [(1, -1), (1, 1)]], dtype=float),
+            np.array([[(-1, -1), (-1, 1)],
+                      [(1, -1), (1, 1)]], dtype=float)]
 """List[ndarray]: Game 7 - A 2-action 2-player game with team rewards.
 
 An example of a game where commitment may be exploited.
 """
 
-monfg8 = [np.array([[(10, 2), (0, 0)],
-                    [(0, 0), (2, 10)]], dtype=float),
-          np.array([[(10, 2), (0, 0)],
-                    [(0, 0), (2, 10)]], dtype=float)]
+payoffs8 = [np.array([[(10, 2), (0, 0)],
+                      [(0, 0), (2, 10)]], dtype=float),
+            np.array([[(10, 2), (0, 0)],
+                      [(0, 0), (2, 10)]], dtype=float)]
 """List[ndarray]: Game 8 - A 2-action 2-player game with team rewards.
 
 There are two NE when both agents use utility function u2 under SER: (L,L) and (R, R).
 The cyclic equilibrium is to mix uniformly over these.
 """
 
-monfg9 = [np.array([[(10, 2), (2, 3)],
-                    [(4, 2), (6, 3)]], dtype=float),
-          np.array([[(10, 2), (2, 3)],
-                    [(4, 2), (6, 3)]], dtype=float)]
+payoffs9 = [np.array([[(10, 2), (2, 3)],
+                      [(4, 2), (6, 3)]], dtype=float),
+            np.array([[(10, 2), (2, 3)],
+                      [(4, 2), (6, 3)]], dtype=float)]
 """List[ndarray]: Game 9 - A 2-action 2-player game with team rewards.
 
 A noisy version of game 8.
 The cyclic equilibrium with utility function u2 is to play A 75% of the time and 25% B.
 """
 
-monfg10 = [np.array([[(2, 0), (0, 1)],
-                     [(1, 0), (0, 2)]], dtype=float),
-           np.array([[(2, 0), (1, 1)],
-                     [(1, 1), (0, 2)]], dtype=float)]
+payoffs10 = [np.array([[(2, 0), (0, 1)],
+                       [(1, 0), (0, 2)]], dtype=float),
+             np.array([[(2, 0), (1, 1)],
+                       [(1, 1), (0, 2)]], dtype=float)]
 """List[ndarray]: Game 10 - A 2-action 2-player game with individual rewards.
 
 This game has no Nash equilibrium with utility functions u1 and u2, but does have a cyclic Nash equilibrium.
 """
 
-monfg11 = [np.array([[(2, 0), (1, 1)],
-                     [(1, 1), (0, 2)]], dtype=float),
-           np.array([[(2, 0), (0, 1)],
-                     [(1, 0), (0, 2)]], dtype=float)]
+payoffs11 = [np.array([[(2, 0), (1, 1)],
+                       [(1, 1), (0, 2)]], dtype=float),
+             np.array([[(2, 0), (0, 1)],
+                       [(1, 0), (0, 2)]], dtype=float)]
 """List[ndarray]: Game 11 - A 2-action 2-player game with individual rewards.
 
 The same game as game 10 but intended to be used with the utility functions reversed.
 """
 
-monfg12 = [
+payoffs12 = [
     np.array([[(4, 1), (1, 2), (2, 1)],
               [(3, 1), (3, 2), (1, 2)],
               [(1, 2), (2, 1), (1, 3)]], dtype=float),
@@ -129,7 +131,7 @@ monfg12 = [
 This game has two PSNE using u1 and u5: [0, 0], [2, 2]. Checked for correctness using Gambit.
 """
 
-monfg13 = [
+payoffs13 = [
     np.array([[(2, 3), (3, 2), (1, 1)],
               [(2, 5), (0, 2), (5, 2)],
               [(1, 3), (4, 0), (1, 3)]], dtype=float),
@@ -141,7 +143,7 @@ monfg13 = [
 This game has no PSNE using u1 and u5. Checked for correctness using Gambit.
 """
 
-monfg14 = [
+payoffs14 = [
     np.array([[[(1, 0), (2, 1)],
                [(3, 0), (1, 2)]],
               [[(0, 2), (2, 2)],
@@ -159,7 +161,7 @@ monfg14 = [
 This game has two PSNE using u1, u5 and u6: [0, 1, 1], [1, 0, 1]. Checked for correctness by hand.
 """
 
-monfg15 = [
+payoffs15 = [
     np.array([[[(1, 0), (2, 1), (1, 2)],
                [(3, 0), (1, 2), (2, 2)]],
               [[(0, 2), (2, 2), (3, 0)],
@@ -183,7 +185,7 @@ monfg15 = [
 This game has three PSNE using u1, u5 and u6: [0, 1, 1], [1, 0, 2], [1, 1, 0]. Checked for correctness by hand.
 """
 
-monfg16 = [
+payoffs16 = [
     np.array([[(2, 0), (1, 0)],
               [(0, 1), (0, 2)]], dtype=float),
     np.array([[(1, 0), (0, 2)],
@@ -193,10 +195,10 @@ monfg16 = [
 This game has no NE when both players use the utility function u1.
 """
 
-monfg17 = [np.array([[(4, 1), (1, 1.5)],
-                     [(3, 1), (3, 2)]], dtype=float),
-           np.array([[(4, 1), (1, 1.5)],
-                     [(3, 1), (3, 2)]], dtype=float)]
+payoffs17 = [np.array([[(4, 1), (1, 1.5)],
+                       [(3, 1), (3, 2)]], dtype=float),
+             np.array([[(4, 1), (1, 1.5)],
+                       [(3, 1), (3, 2)]], dtype=float)]
 """List[ndarray]: Game 17 - A 2-action 2-player game with team rewards.
 
 There are two NE under SER using u1 and u2: (L, L) and (M, M).
@@ -206,12 +208,12 @@ This game shows cyclic behaviour under IBR with simultaneous updates but not wit
 This game shows no cyclic behaviour with fictitious play.
 """
 
-monfg18 = [np.array([[(4, 1), (1, 1.5), (2, 1)],
-                     [(3, 1), (3, 2), (1, 2)],
-                     [(1, 2), (2, 1.5), (1.5, 3)]], dtype=float),
-           np.array([[(4, 1), (1, 1.5), (2, 1)],
-                     [(3, 1), (3, 2), (1, 2)],
-                     [(1, 2), (2, 1.5), (1.5, 3)]], dtype=float)]
+payoffs18 = [np.array([[(4, 1), (1, 1.5), (2, 1)],
+                       [(3, 1), (3, 2), (1, 2)],
+                       [(1, 2), (2, 1.5), (1.5, 3)]], dtype=float),
+             np.array([[(4, 1), (1, 1.5), (2, 1)],
+                       [(3, 1), (3, 2), (1, 2)],
+                       [(1, 2), (2, 1.5), (1.5, 3)]], dtype=float)]
 """List[ndarray]: Game 18 - A 3-action 2-player game with team rewards.
 
 There are three NE under SER using u1 and u2: (M, M), (L, L) and (R, R).
@@ -221,10 +223,10 @@ There are three PSNE using u1 and u5: [0, 0], [1, 1], [2, 2]. Checked for correc
 This game shows cyclic behaviour under IBR with simultaneous updates but not with alternating updates.
 """
 
-monfg19 = [np.array([[(7, 12), (3, 4)],
-                     [(11, 7), (7, 3)]], dtype=float),
-           np.array([[(6, 2), (5, 4)],
-                     [(2, 8), (7, 2)]], dtype=float)]
+payoffs19 = [np.array([[(7, 12), (3, 4)],
+                       [(11, 7), (7, 3)]], dtype=float),
+             np.array([[(6, 2), (5, 4)],
+                       [(2, 8), (7, 2)]], dtype=float)]
 """List[ndarray]: Game 19 - A 2-action 2-player game with individual rewards.
 
 This game has a NE of {(0.5, 0.5), (0.75, 0.25)} using u2 for both players. 
@@ -233,54 +235,53 @@ It is used to test the correctness of the FP and IBR algorithms.
 
 
 def get_monfg(game):
-    """Get the payoffs for a game from a string.
+    """Get a predefined MONFG.
 
     Args:
         game (str): The string of the game.
 
     Returns:
         List[ndarray]: A list of payoff matrices.
-
     """
     if game == 'game1':
-        monfg = monfg1
+        payoffs = payoffs1
     elif game == 'game2':
-        monfg = monfg2
+        payoffs = payoffs2
     elif game == 'game3':
-        monfg = monfg3
+        payoffs = payoffs3
     elif game == 'game4':
-        monfg = monfg4
+        payoffs = payoffs4
     elif game == 'game5':
-        monfg = monfg5
+        payoffs = payoffs5
     elif game == 'game6':
-        monfg = monfg6
+        payoffs = payoffs6
     elif game == 'game7':
-        monfg = monfg7
+        payoffs = payoffs7
     elif game == 'game8':
-        monfg = monfg8
+        payoffs = payoffs8
     elif game == 'game9':
-        monfg = monfg9
+        payoffs = payoffs9
     elif game == 'game10':
-        monfg = monfg10
+        payoffs = payoffs10
     elif game == 'game11':
-        monfg = monfg11
+        payoffs = payoffs11
     elif game == 'game12':
-        monfg = monfg12
+        payoffs = payoffs12
     elif game == 'game13':
-        monfg = monfg13
+        payoffs = payoffs13
     elif game == 'game14':
-        monfg = monfg14
+        payoffs = payoffs14
     elif game == 'game15':
-        monfg = monfg15
+        payoffs = payoffs15
     elif game == 'game16':
-        monfg = monfg16
+        payoffs = payoffs16
     elif game == 'game17':
-        monfg = monfg17
+        payoffs = payoffs17
     elif game == 'game18':
-        monfg = monfg18
+        payoffs = payoffs18
     elif game == 'game19':
-        monfg = monfg19
+        payoffs = payoffs19
     else:
         raise Exception("The provided game does not exist.")
 
-    return monfg
+    return Monfg(payoffs)
