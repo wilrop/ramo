450dce398dab5909cd0090e8a4990ccb367ffd85
Willem Röpke <willem.ropke@vub.be>
2023-05-03 15:51:18 +0200

    Add optimised versions of PPrune and CPrune

--- ramo/pareto/verify.py
+++ ramo/pareto/verify.py
@@ -1,8 +1,10 @@
 import numpy as np
-import scipy.optimize as scopt
+from scipy.optimize import linprog, OptimizeWarning
+from scipy.spatial import ConvexHull
 from pulp import *
 
 from ramo.strategy.best_response import calc_expected_returns
+from ramo.pareto.dominance import pareto_dominates
 
 
 def in_hull(x, points):
@@ -20,90 +22,9 @@ def in_hull(x, points):
     A = np.r_[points.T, np.ones((1, n_points))]  # Add row of ones such that the strategy sums to 1.
     b = np.r_[x, np.ones(1)]  # The strategy array.
     with warnings.catch_warnings():
-        warnings.filterwarnings('ignore', category=scopt.OptimizeWarning)  # Suppress full row rank warnings.
-        lp = scopt.linprog(c, A_eq=A, b_eq=b)  # Check if we can find a convex combination by linear programming.
+        warnings.filterwarnings('ignore', category=OptimizeWarning)  # Suppress full row rank warnings.
+        lp = linprog(c, A_eq=A, b_eq=b)  # Check if we can find a convex combination by linear programming.
     return lp.success
-
-
-def c_prune(candidates):
-    """Create a convex coverage set from a set of candidate points.
-
-    References:
-        .. [1] Roijers, D. M., & Whiteson, S. (2017). Multi-objective decision making. 34, 129–129.
-            https://doi.org/10.2200/S00765ED1V01Y201704AIM034
-
-    candidates (Set[Tuple]): A set of vectors.
-
-    Returns:
-        Set[Tuple]: A convex coverage set.
-
-    """
-    num_objectives = len(list(candidates)[0])
-    zero_vec = tuple(np.zeros(num_objectives))
-    p_candidates = p_prune(candidates)
-    ccs = {zero_vec}
-
-    while p_candidates:
-        vector = p_candidates.pop()  # Get an element from the Pareto coverage set.
-        p_candidates.add(vector)  # Pop removes the element so add it back for now.
-
-        weight = find_weight(vector, ccs)  # Get a weight for which this vector improves the CCS.
-
-        if weight is None:  # If there is none, discard the vector.
-            p_candidates.remove(vector)
-        else:  # Otherwise add the best vector to the CCS.
-            new_vector = max(p_candidates, key=lambda x: np.dot(x, weight))
-            p_candidates.remove(new_vector)
-            ccs.add(new_vector)
-
-    ccs.remove(zero_vec)
-    return ccs
-
-
-def pareto_dominates(a, b):
-    """Check if the vector in a Pareto dominates vector b.
-
-    Args:
-        a (ndarray): A numpy array.
-        b (ndarray): A numpy array.
-
-    Returns:
-        bool: Whether vector a dominates vector b.
-    """
-    a = np.array(a)
-    b = np.array(b)
-    return np.all(a >= b) and np.any(a > b)
-
-
-def p_prune(candidates):
-    """Create a Pareto coverage set from a set of candidate points.
-
-    References:
-        .. [1] Roijers, D. M., & Whiteson, S. (2017). Multi-objective decision making. 34, 129–129.
-            https://doi.org/10.2200/S00765ED1V01Y201704AIM034
-
-    Args:
-        candidates (Set[Tuple]): A set of vectors.
-
-    Returns:
-        Set[Tuple]: A Pareto coverage set.
-    """
-    pcs = set()
-    while candidates:
-        vector = candidates.pop()
-
-        for alternative in candidates:
-            if pareto_dominates(alternative, vector):
-                vector = alternative
-
-        to_remove = set(vector)
-        for alternative in candidates:
-            if pareto_dominates(vector, alternative):
-                to_remove.add(alternative)
-
-        candidates -= to_remove
-        pcs.add(vector)
-    return pcs
 
 
 def find_weight(vector, candidates):
@@ -146,6 +67,108 @@ def find_weight(vector, candidates):
     if success and x > 0:
         return weight_vec
     return None
+
+
+def c_prune(candidates):
+    """Create a convex coverage set from a set of candidate points.
+
+    References:
+        .. [1] Roijers, D. M., & Whiteson, S. (2017). Multi-objective decision making. 34, 129–129.
+            https://doi.org/10.2200/S00765ED1V01Y201704AIM034
+
+    candidates (Set[Tuple]): A set of vectors.
+
+    Returns:
+        Set[Tuple]: A convex coverage set.
+
+    """
+    num_objectives = len(list(candidates)[0])
+    zero_vec = tuple(np.zeros(num_objectives))
+    p_candidates = p_prune(candidates)
+    ccs = {zero_vec}
+
+    while p_candidates:
+        vector = p_candidates.pop()  # Get an element from the Pareto coverage set.
+        p_candidates.add(vector)  # Pop removes the element so add it back for now.
+
+        weight = find_weight(vector, ccs)  # Get a weight for which this vector improves the CCS.
+
+        if weight is None:  # If there is none, discard the vector.
+            p_candidates.remove(vector)
+        else:  # Otherwise add the best vector to the CCS.
+            new_vector = max(p_candidates, key=lambda x: np.dot(x, weight))
+            p_candidates.remove(new_vector)
+            ccs.add(new_vector)
+
+    ccs.remove(zero_vec)
+    return ccs
+
+
+def p_prune(candidates):
+    """Create a Pareto coverage set from a set of candidate points.
+
+    References:
+        .. [1] Roijers, D. M., & Whiteson, S. (2017). Multi-objective decision making. 34, 129–129.
+            https://doi.org/10.2200/S00765ED1V01Y201704AIM034
+
+    Args:
+        candidates (Set[Tuple]): A set of vectors.
+
+    Returns:
+        Set[Tuple]: A Pareto coverage set.
+    """
+    pcs = set()
+    while candidates:
+        vector = candidates.pop()
+
+        for alternative in candidates:
+            if pareto_dominates(alternative, vector):
+                vector = alternative
+
+        to_remove = set(vector)
+        for alternative in candidates:
+            if pareto_dominates(vector, alternative):
+                to_remove.add(alternative)
+
+        candidates -= to_remove
+        pcs.add(vector)
+    return pcs
+
+
+def fast_c_prune(candidates):
+    """A fast version to prune a set of points to its convex hull. This leverages the QuickHull algorithm.
+
+    This algorithm first computes the convex hull of the set of points and then prunes the Pareto dominated points.
+
+    Args:
+        candidates (ndarray): A numpy array of vectors.
+
+    Returns:
+        ndarray: A convex coverage set.
+    """
+    hull = ConvexHull(candidates)
+    ccs = candidates[hull.vertices]
+    return fast_p_prune(ccs)
+
+
+def fast_p_prune(candidates):
+    """A batched and fast version of the Pareto coverage set algorithm.
+
+    Args:
+        candidates (ndarray): A numpy array of vectors.
+
+    Returns:
+        ndarray: A Pareto coverage set.
+    """
+    candidates = np.unique(candidates, axis=0)
+    if len(candidates) == 1:
+        return candidates
+
+    res_eq = np.all(candidates[:, None, None] <= candidates, axis=-1).squeeze()
+    res_g = np.all(candidates[:, None, None] < candidates, axis=-1).squeeze()
+    c1 = np.sum(res_eq, axis=-1) == 1
+    c2 = np.any(~res_g, axis=-1)
+    return candidates[np.logical_and(c1, c2)]
 
 
 def verify_pareto_nash(monfg, joint_strat):
